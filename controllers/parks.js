9c173086ea6e25399f4c784f3e5a24e1242ef3a6
Atomic82 <tom@atomic82.com>
2022-03-07 15:59:59 -0500

    Add emoji

--- controllers/parks.js
+++ controllers/parks.js
@@ -2,6 +2,13 @@ import { Park } from "../models/park.js"
 
 function index(req, res) {
   console.log('Parks')
+  Park.find({})
+  .then(parks => {
+    res.render('parks/index', {
+      parks,
+      titles: "🏞"
+    })
+  })
 }
 
 export {
