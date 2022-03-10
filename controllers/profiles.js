5ba0edecbedd70eecf1653ce11e47e90bb713c11
Atomic82 <tom@atomic82.com>
2022-03-09 19:16:39 -0500

    Create profiles view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,7 +1,16 @@
 import { Profile } from "../models/profile.js"
 
 function index(req, res) {
-  console.log("Profiles")
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      profiles,
+      title: "🌄"
+    })
+  })
+  .catch(err => {
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
 }
 
 export {
